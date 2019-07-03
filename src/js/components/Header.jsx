063789de6ed26935dabfab60200d45fde1f9ec50
ilsa malik <ilsamalik344@gmail.com>
2019-07-03 01:04:04 -0700

    initialize

---
+++ src/js/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends React.Component {
+    // state = {  }
+    render() { 
+        return ( 
+            <div>
+                hello
+            </div>
+         );
+    }
+}
+ 
+export default Header;
---
+++ src/js/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from "./components/App";
+
+ReactDOM.render(
+   <div>dfgdfg</div>,
+document.querySelector("#app")
+    );
+
